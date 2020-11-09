76e011dceb2f6b064019281c4cadd77ca51fee63
CatanaRaulAndrei <raul19@windowslive.com>
2020-11-09 15:25:21 +0200

    Servicii Cloud

---
+++ main.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true
+    });
+    $('.modal').modal();
+    $('.tabs').tabs();
+    $('select').formSelect();
+});
